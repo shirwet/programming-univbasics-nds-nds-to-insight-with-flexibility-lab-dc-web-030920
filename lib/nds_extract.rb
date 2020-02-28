becdf74929b1de5134f41e7854fd123aeb0c8dfe
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-28 00:24:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -54,6 +54,17 @@ def movies_with_director_key(name, movies_collection)
   :worldwide_gross => movie_data[:worldwide_gross],
   :release_year => movie_data[:release_year],
   
+  result = []
+   index = 0
+
+    while index < movies_collection.length do
+     movie_data = movies_collection[index]
+     result << movie_with_director_name(name, movie_data)
+     index += 1
+   end
+
+    result
+  
 end
 
 
